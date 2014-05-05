44ebdb519bfa42e3576fddcad2b5a83bc1b239c7
Lorenzo Tello <ltello8a@gmail.com>
2014-05-05 20:47:28 +0200

    better checking of invalid talking token response

--- lib/service/connection/response.rb
+++ lib/service/connection/response.rb
@@ -13,6 +13,9 @@ module Service
         #
         # @return [false, Object]: an error code or false if not invalid response.
         def invalid_talking_token_response?(resp)
+          REMOTE_SERVICE_INVALID_TALKING_TOKEN_ERROR_CODES.each do |code|
+            return code if resp.headers['www-authenticate'] =~ Regexp.new(code)
+          end if resp.respond_to?(:headers)
           return false unless (resp.respond_to?(:error) and resp.error)
           return resp.error.code if REMOTE_SERVICE_INVALID_TALKING_TOKEN_ERROR_CODES.include?(resp.error.code)
           false
